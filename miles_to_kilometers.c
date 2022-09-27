0c2d9b6a5b9721fb6d0d85bdfb7038b021f5bd74
fadhil daeng naba <69493052+fdaengnaba@users.noreply.github.com>
2022-09-27 21:39:57 +0700

    implement get distance in mile

---
+++ miles_to_kilometers.c
@@ -0,0 +1,19 @@
+/*
+* Converts distance in miles to kilometers.
+*/
+
+#include <stdio.h>
+#define KMS_PER_MILE 1.609
+
+int main(void)
+{
+    double miles, // input - distance in miles.
+     kms; // output - distance in kilometers
+
+     /* Get the distance in miles. */
+     pritnf("%s", "Enter the distance in miles> ");
+     scanf("%lf", &miles);
+
+     /* Convert the distance to kilometers. */
+     
+}
