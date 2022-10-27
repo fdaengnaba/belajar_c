f5ff93fa4c03b6776532a1a864cb934b121b3b9d
fadhil daeng naba <69493052+fdaengnaba@users.noreply.github.com>
2022-10-27 21:41:33 +0700

    update cutgrasseta

--- 2_overviewOfC/cutgrasseta.c
+++ 2_overviewOfC/cutgrasseta.c
@@ -2,5 +2,23 @@
 
 int main(void)
 {
-    
+    /* Get length and width of yard and house. */
+    int yard_length, yard_width;
+    printf("Enter yard length> ");
+    scanf(" %d", &yard_length);
+    printf("Enter yard width> ");
+    scanf(" %d", &yard_width);
+
+    int house_length, house_width;
+    printf("Enter house length> ");
+    scanf(" %d", &house_length);
+    printf("Enter house width> ");
+    scanf(" %d", &house_width);
+
+    /* Calculate time required to cut grass. */
+    long int area = (yard_length * yard_width) - (house_length * house_width);
+    long int time = area / 2;
+
+    /* Display result. */
+    printf("Time required is %ld second(s)\n", time);
 }
