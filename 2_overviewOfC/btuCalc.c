bb75269b74b326ca9c72f45074a3106336b26c8d
fadhil daeng naba <69493052+fdaengnaba@users.noreply.github.com>
2022-10-27 19:00:44 +0700

    update btuCalc.c

--- 2_overviewOfC/btuCalc.c
+++ 2_overviewOfC/btuCalc.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#define BTU_GALLON  138095.2381
 
 int main(void)
 {
-    
+    printf("BTU calculator bprogram\n");
+
+    /* Get efficiency and gallons of oil. */
+    int efficiency;
+    printf("Enter thermal efficiency in percent)> ");
+    scanf(" %d", &efficiency);
+
+    int oil;
+    printf("Enter how many gallons of oil> ");
+    scanf(" %d", &oil);
+
+    /* Calculate BTU. */
+    double btu = (double) oil * BTU_GALLON * efficiency / 100;
+     
+    /* Display resultl. */
+    printf("BTU: %.2lf\n", btu);
 
     return 0;
 }
