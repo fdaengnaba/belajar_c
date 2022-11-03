b8f6a16b28278d6f2009dbafa2054c273dce579e
fadhil daeng naba <69493052+fdaengnaba@users.noreply.github.com>
2022-11-03 17:39:46 +0700

    add perpindicular.c

---
+++ 2_overviewOfC/perpindicular.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct point_s {
+    double x;
+    double y;
+} point;
+
+int main(void)
+{
+    /* Prompt for and input the coordinates of the two points. */
+    point a, b;
+    printf("Input two point\n");
+    printf("point A> ");
+    scanf(" %lf %lf", a.x, a.y);
+    printf("point B> ");
+    scanf(" %lf %lf", b.x, b.y);
+
+    /* Compute the slope of the line between those two points. */
+    
+
+    return 0;
+}
