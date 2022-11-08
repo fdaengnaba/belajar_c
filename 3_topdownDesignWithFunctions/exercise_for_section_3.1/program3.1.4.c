3ed73e11f33f44a530a794488f8883f87eb6e3a2
fadhil daeng naba <69493052+fdaengnaba@users.noreply.github.com>
2022-11-08 09:34:49 +0700

    add program 3.1.4.c

---
+++ 3_topdownDesignWithFunctions/exercise_for_section_3.1/program3.1.4.c
@@ -0,0 +1,14 @@
+/*
+ * Computes the weight of a batch of flat washers.
+ */
+
+#include <stdio.h> /* printf, scanf, definitions */
+#define PI 3.14159
+
+int main(void)
+{
+    double hole_diameter; /* input - diameter of hole       */
+    double edge_diameter; /* input - diamater of outer edge */
+    double thickness;     /* input - thickness of washer    */
+    
+}
