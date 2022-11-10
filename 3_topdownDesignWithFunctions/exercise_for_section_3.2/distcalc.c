623cfa34404dad116a5c3742f61c415af74c4624
fadhil daeng naba <69493052+fdaengnaba@users.noreply.github.com>
2022-11-10 10:36:52 +0700

    add distcalc.c

---
+++ 3_topdownDesignWithFunctions/exercise_for_section_3.2/distcalc.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef struct point_s {
+    double x;
+    double y;
+    double z;
+} point;
+
+int main(void)
+{
+    point satu, dua; /* input - point 3-D coordinats         */
+    double distance; /* output - distance between two points */
+
+    /* Get two points coordinates. */
+    printf("Enter point one> ");
+    scanf("%lf %lf %lf", &satu.x, &satu.y, &satu.z);
+    printf("Enter point two> ");
+    scanf(" %lf %lf %lf", &dua.x, &dua.y, &dua.z);
+
+    /* Compute the distance between two ponts. */
+    
+
+    return 0;
+}
