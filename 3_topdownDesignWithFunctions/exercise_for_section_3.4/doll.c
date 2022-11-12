c6e7758277eb07f2ee5ab5cd3174bf6d6cb01a64
fadhil daeng naba <69493052+fdaengnaba@users.noreply.github.com>
2022-11-12 18:25:56 +0700

    add doll.c
    add function prototype

---
+++ 3_topdownDesignWithFunctions/exercise_for_section_3.4/doll.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void print_d(void); /* Print 'D'. */
+void print_o(void); /* Print 'O'. */
+void print_l(void); /* Print 'L'. */
+
+int main(void)
+{
+    
+
+    return 0;
+}
