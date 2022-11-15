9b37e2d3f0b2976634a6ffeba16fe8bfe870bcfa
fadhil daeng naba <69493052+fdaengnaba@users.noreply.github.com>
2022-11-15 14:39:05 +0700

    add cone_total_surface.c
    initialize source code,
    add variabel declaration

---
+++ 3_topdownDesignWithFunctions/cone_total_surface.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+
+void instruct(void); /* Display instruction. */
+/* 
+ * Calculate total surface area of a cone
+ * given its base radius and slant height
+ */
+double find_total_surface_area(double r, double s);
+
+int main(void)
+{
+    double base_radius;        /* input - base radius of the cone  */
+    double slant_height;       /* input - slant heigth of the cone */
+    double total_surface_area; /* output - tot surf area of cone   */
+
+    /* Display instruction. */
+    instruct();
+    
+    /* Get base radius, and slant height. */
+}
