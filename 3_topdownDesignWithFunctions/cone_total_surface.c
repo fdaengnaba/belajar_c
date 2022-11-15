09c5444cc29d1f3271172dc7f21451ed877d7734
fadhil daeng naba <69493052+fdaengnaba@users.noreply.github.com>
2022-11-15 14:52:08 +0700

    update cone_total_surface.c
    implement double find_total_surface_area(double r, bouble s)

--- 3_topdownDesignWithFunctions/cone_total_surface.c
+++ 3_topdownDesignWithFunctions/cone_total_surface.c
@@ -1,4 +1,5 @@
 # include <stdio.h>
+#define PI 3.14159265359
 
 void instruct(void); /* Display instruction. */
 /* 
@@ -39,4 +40,13 @@ void instruct(void)
     printf("Program to calculate total surface area\n");
     printf("of a cone given the base radius and \n");
     printf("slant height of the cone\n\n");
+}
+
+/* 
+ * Calculate total surface area of a cone
+ * given its base radius and slant height
+ */
+double find_total_surface_area(double r, double s)
+{
+    return PI * r * (r + l);
 }
