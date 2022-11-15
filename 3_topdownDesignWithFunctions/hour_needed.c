db2e4db4ecce7edcfba0152fa6a9b61c73cfa587
fadhil daeng naba <69493052+fdaengnaba@users.noreply.github.com>
2022-11-15 20:08:04 +0700

    add hour_needed.c
    add function prototype

---
+++ 3_topdownDesignWithFunctions/hour_needed.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+double find_income_gap(double monthly_install, double monthly_payment);
+double find_hour_needed(double income_gap, double hourly_rate);
+
+int main(void)
+{
+    
+}
