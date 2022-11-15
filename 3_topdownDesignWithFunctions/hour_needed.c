cc1e01e6f5231b7e2a90846bac605b8cd7360780
fadhil daeng naba <69493052+fdaengnaba@users.noreply.github.com>
2022-11-15 20:25:12 +0700

    update hour_needed.c
    add variabel declaration

--- 3_topdownDesignWithFunctions/hour_needed.c
+++ 3_topdownDesignWithFunctions/hour_needed.c
@@ -5,5 +5,11 @@ double find_hour_needed(double income_gap, double hourly_rate);
 
 int main(void)
 {
+    double monthly_payment; /* input - monthly income from project */
+    double hourly_rate;     /* input - hourly income rate* parttime*/
+    double monthly_install; /* input - monthly install must paid   */
+    double work_hours;      /* output - hours of woek needed       */
+    double install_gap;     /* gap between install and project mny */
     
+    return 0;
 }
