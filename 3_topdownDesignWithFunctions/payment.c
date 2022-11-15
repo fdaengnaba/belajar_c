ac866ad605a7ad41c90f566ccd048a5dc20dcf8a
fadhil daeng naba <69493052+fdaengnaba@users.noreply.github.com>
2022-11-15 11:26:13 +0700

    add payment.c
    add variable declaration

---
+++ 3_topdownDesignWithFunctions/payment.c
@@ -0,0 +1,27 @@
+/*
+ * Program that calculate monthly payment, given the purchase price,
+ * down payment, annual interest rate, and number of payments
+ */
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    double purchase_price;        /* input - purchase price           */
+    double down_payment;          /* input - down payment             */
+    double annual_interest_rate;  /* input - interest rate for a year */
+    int number_of_payment;        /* input - number of payment        */
+    double amount_borowwed;       /* output - amount borowwed         */
+    double monthly_payment;       /* output - monthly payment         */
+    double monthly_interest_rate; /* 1/12 annual interest rate        */
+
+    /* 
+     * Get purchase_price, down_payment, annual_interest_rate
+     * number_of_payments
+     */
+    printf("Enter the purchase price> $ ");
+    scanf("%lf", &purchase_price);
+    printf("")
+
+    return 0;
+}
